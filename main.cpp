3bf7b827f31d4921d0df63e10d7aae49c7b3f651
Болдырев Артём <ceme4k4@mail.ru>
2021-10-30 10:17:24 +0300

    Realize IsLeapYear function

--- main.cpp
+++ main.cpp
@@ -4,3 +4,16 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
+
+bool IsLeapYear(int year)
+{
+    if (year % 4 == 0)
+        if (year % 100 == 0)
+            if (year % 400 == 0)
+                return true;
+            else
+                return false;
+        else
+            return true;
+    else return false;
+}
